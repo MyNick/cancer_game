7e8e7740d138c90b7c683c22756409210de61aa2
me <me>
2019-04-12 04:00:19 +0300

    GUIII

---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour
+{
+    public Image health_background;
+    public Image health_foreground;
+    public Image cells_background;
+    public Image cells_foreground;
+    public GameObject player;
+
+    public const float MAX_CANCER = 100;
+
+    void Start()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        float cancer = GameObject.FindGameObjectsWithTag("Cancer").Length;
+        cancer += GameObject.FindGameObjectsWithTag("WallCancer").Length;
+        cancer /= MAX_CANCER;
+        cells_foreground.fillAmount = cancer;
+    }
+}
